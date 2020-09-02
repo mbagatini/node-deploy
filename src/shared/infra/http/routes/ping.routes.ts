54f9d3f0f2774c6ec56b5c1058c67ad9b8fc8247
Morgana Bagatini <morganabagatini@gmail.com>
2020-09-02 12:48:00 -0300

    Update ping.routes.ts

--- src/shared/infra/http/routes/ping.routes.ts
+++ src/shared/infra/http/routes/ping.routes.ts
@@ -3,6 +3,6 @@ import { Router } from "express";
 const pingRouter = Router();
 
 pingRouter.get("/", async (request, response) => {
-    return response.json({ message: "pong - Hiiii Lorenaaaaa" });
+    return response.json({ message: "pong - teste" });
 });
 export default pingRouter;
